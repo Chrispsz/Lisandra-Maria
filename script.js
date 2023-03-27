7f72e6256b9a3be07af0a6beb4024f223c7ee824
Chrispsz <113193612+Chrispsz@users.noreply.github.com>
2023-03-27 20:31:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+	<p>Clique no botão e veja.</p>
+
+	<button onclick="myFunction()">Clique aqui Lisandra</button>
+
+	<script>
+		function myFunction() {
+			
+			alert("Te amo♥️!");
+			
+		}
+	</script>
+
+</body>
+</html>
